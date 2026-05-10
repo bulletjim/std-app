21a723800a0bed85fe6e9055b249f84f3d7e755a
BulletJim <gianmarco.campania@gmail.com>
2026-05-10 21:26:09 +0200

    Landing Page prototype Implemented

--- src/renderer.ts
+++ src/renderer.ts
@@ -28,6 +28,27 @@
 
 import './index.css';
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite',
-);
+const createTableDialog = document.getElementById("create-table-dialog") as HTMLDialogElement;
+const createTblButton = document.getElementById("create-table-button") as HTMLButtonElement;
+createTblButton.addEventListener("click", () =>{
+  createTableDialog.showModal()
+})
+
+const closeTableButton = document.getElementById("close-create-table-dialog-button") as HTMLButtonElement;
+closeTableButton.addEventListener("click", () =>{
+  createTableDialog.returnValue = "";
+  createTableDialog.close()
+})
+
+const createTableForm = document.getElementById("create-table-form") as HTMLFormElement;
+const inputName = document.getElementById("input-table-name") as HTMLInputElement;
+const inputPassword = document.getElementById("input-table-password") as HTMLInputElement;
+createTableForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const name = inputName.value;
+  const pwd = inputPassword.value;
+  console.log(name, pwd)
+  createTableForm.reset()
+  createTableDialog.close()
+})
+
